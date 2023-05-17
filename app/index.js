132f63302c68dca9b4b6adb741adafffdb6a8e0e
Ubeydu <u.keles@gmail.com>
2023-05-17 19:41:44 +0300

    Project is ready to do JS exercies.

--- app/index.js
+++ app/index.js
@@ -1,2 +1,103 @@
-console.log('hello webpack');
-console.log('hello in the browser.');
+// let a = 'hello';
+// console.log(a);
+
+// {
+//     let a = 'goodbye';
+//     console.log('a inside scope')
+// }
+
+// const an_array = [1, 2, 3];
+// an_array.push(4);
+// console.log(an_array)
+
+// var a = 'hello';
+// var b = 'world';
+
+
+// let d = `hello ${b}`;
+// console.log(d);
+
+// let a = [7, 8, 9];
+// let b = [6, ...a, 10];
+// console.log(b)
+
+// function ekranabas(...z) {
+//     console.log(z);
+// }
+
+// ekranabas(1, 2, 3, 4, 5);
+
+// function butter() {
+//     let a = [1, 2, 3];
+//     console.log(a);
+// }
+
+// butter(4, 5, 6);
+
+// function butter(...z) {
+//     let a = [1, 2, 3, ...z];
+//     console.log(a);
+//   }
+
+//   butter(4, 5, 6);
+
+// let c = [100, 200]
+// let [a, b] = c;
+// console.log(b, b, a)
+
+// let c = [100, 200, 300, 400, 500];
+// let [a, ...b] = c;
+// console.log(b)
+
+// let ranger = {magical: false, power: 9};
+// let {magical, power} = ranger;
+// console.log(magical, power);
+
+// function blastoff() {
+//   console.log('3...2...1... blastoff!')
+// }
+
+// blastoff();
+
+// setTimeout(() => {
+//   console.log('3...2...1... blastoff!')
+// }, 3000);
+
+// this.a = 25;
+
+// let print = function() {
+//   this.a = 50;
+//   console.log('this.a', this.a);
+// }
+// print();
+
+// let arrowPrint = () => {
+//   console.log(this.a);
+// }
+
+// arrowPrint();
+
+// let points = [10, 20, 30];
+
+// points = points.map(element => element +1);
+
+// console.log(points);
+
+// let lengths = [3, 7, 5];
+// let multiple = 8;
+
+// const scale = (multiple) => {
+//   return lengths.map(element => element*multiple);
+// };
+
+// console.log(scale(2))
+
+let isPassing = (grade) => {
+  return grade >= 70;
+}
+
+let scores = [90, 85, 67, 71, 70, 55, 92];
+
+let passing = scores.filter(isPassing);
+
+console.log(passing);
